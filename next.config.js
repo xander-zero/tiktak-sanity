99d925b9c131d0fae92d80003ab5157edfc34da0
xander-zero <mahmud.ansari60636@gmail.com>
2022-09-04 21:11:15 +0430

    create videoCard components

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["pps.whatsapp.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
